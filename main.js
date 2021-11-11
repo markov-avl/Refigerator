3f7db73b96045b6de0b32ff67a794fee47d2f0dd
markov-avl <andrewshasucks@gmail.com>
2021-11-11 18:48:37 +1000

    some bug fixes

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const COMPARTMENT = document.getElementById('compartment')
 const LATTICE = document.getElementById('lattice')
 const SIZE = 4
 
+let listeners = []
+for (let i = 1; i <= SIZE; i++) {
+    listeners.push(null);
+}
+
 
 COMPARTMENT.style.gridTemplate = `
     repeat(${SIZE}, ${(window.outerHeight * 0.7 - (SIZE * 10)) / SIZE}px) / 
@@ -12,15 +17,18 @@ LATTICE.style.gridTemplate = `repeat(${SIZE}, 1fr) / repeat(${SIZE}, 1fr)`
 
 
 function isWon() {
+    console.log('isWon')
     let beer = document.getElementsByClassName("beer vertical")
     if (beer.length === 0 || beer.length === SIZE ** 2) {
-        alert("You won!")
+        removeListeners()
+        setTimeout('alert("You won!"); regenerateCompartment()', 500);
         return true
     }
     return false
 }
 
 function generateCompartment() {
+    console.log('generateCompartment')
     for (let i = 0; i < SIZE ** 2; ++i) {
         COMPARTMENT.innerHTML += `<div class="beer ${Math.floor(Math.random() * 2) ? 'vertical' : 'horizontal'}" 
                                        data-i="${Math.floor(i / SIZE)}" 
@@ -28,29 +36,38 @@ function generateCompartment() {
                                        <img src="images/beer.png" alt="">
                                   </div>`
     }
-    if (isWon()) {
-        generateCompartment()
-    }
-}
-
-function regenerateCompartment() {
-    COMPARTMENT.innerHTML = ''
-    generateCompartment()
     addListeners()
 }
 
+function regenerateCompartment() {
+    console.log('regenerateCompartment')
+    COMPARTMENT.innerHTML = ''
+    generateCompartment()
+}
+
 function generateLattice() {
+    console.log('generateLattice')
     for (let i = 0; i < SIZE ** 2; ++i) {
         LATTICE.innerHTML += '<div class="ventilation"></div>'
     }
 }
 
 function addListeners() {
+    console.log('addListeners')
     for (let k = 0; k < SIZE ** 2; ++k) {
         let i = Math.floor(k / SIZE)
         let j = k % SIZE
-        document.querySelector(`[data-i="${i}"][data-j="${j}"]`).addEventListener('click',
-            reverse.bind(null, i, j))
+        listeners[k] = () => { reverse(i, j) }
+        document.querySelector(`[data-i="${i}"][data-j="${j}"]`).addEventListener('click', listeners[k])
+    }
+}
+
+function removeListeners() {
+    console.log('removeListeners')
+    for (let k = 0; k < SIZE ** 2; ++k) {
+        let i = Math.floor(k / SIZE)
+        let j = k % SIZE
+        document.querySelector(`[data-i="${i}"][data-j="${j}"]`).removeEventListener('click', listeners[k])
     }
 }
 
@@ -76,5 +93,4 @@ function reverse(row, column) {
 
 generateCompartment()
 generateLattice()
-addListeners()
 SIGNBOARD.addEventListener('click', regenerateCompartment)
